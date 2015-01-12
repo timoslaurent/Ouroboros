433106fefc55dd9ecbd73e3fbf436c833277d6fb
timoslaurent <timoslaurent@yahoo.fr>
2015-01-12 22:02:49 +0100

    Ouroboros_camera/controller
    Camera isométrique + Contrôle du player à la manette (sphère). Mais la
    sphère bouge tout seul étrangement

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		camera.orthographic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		camera.orthographicSize = 5;
+	}
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	float speed, mX, moveX, mY, moveY;
+
+	// Use this for initialization
+	void Start () {
+		speed = 20;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+	}
+
+	void Movement () {
+		//Left Joystick
+		mX = Input.GetAxis ("L_Horizontal") * speed * Time.deltaTime;
+		transform.Translate (mX, 0, 0);
+		mY = Input.GetAxis ("L_Vertical") * speed * Time.deltaTime;
+		transform.Translate (0, mY, 0);
+
+		//Right Joystick
+		moveX = Input.GetAxis ("R_Horizontal") * speed * Time.deltaTime;
+		transform.Translate (moveX, 0, 0);
+		moveY = Input.GetAxis ("R_Vertical") * speed * Time.deltaTime;
+		transform.Translate (0, moveY, 0);
+	}
+}
